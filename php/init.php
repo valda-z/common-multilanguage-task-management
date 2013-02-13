f58c7024b8e7767dbb7f025d86d9b9b42561097d
Trent Swanson <trent@fullscale180.com>
2013-02-13 15:45:05 -0800

    cleanup

--- php/init.php
+++ php/init.php
@@ -7,15 +7,15 @@ use WindowsAzure\Common\ServiceException;
 
 $connectionString = getenv("STORAGE_CONNECTION");
 if ($connectionString == null)
-    $connectionString = "DefaultEndpointsProtocol=http;AccountName=zim;AccountKey=0c26sS8E+Nz1BJmgWVRLMEUPKPwa1lt3jvdjjXgZQ/FVdjKJHGl2GV96jVJvciV8In290PXuNLPZBPc0AOvb2g==";
+    $connectionString = "";
 
 $sbConnectionString = getenv("SB_CONNECTION");
 if ($sbConnectionString == null)
-    $sbConnectionString = "Endpoint=https://invader.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=7TkK9JqQXbLdy5fH9mhEivU+Js4RpsVI9TaugN6/zdA=";
+    $sbConnectionString = "Endpoint=";
 
 $sbEnable = getenv("SB_ENABLE");
 if ($sbEnable == null)
-    $sbEnable = false;
+    $sbEnable = true;
 else
     $sbEnable = ($sbEnable && strtolower($sbEnable) == "true");
 
